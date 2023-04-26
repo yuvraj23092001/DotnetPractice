27ae14b1f86699a4505392bb6e147a788dfb6953
Yuvraj <khayuvraj@gmail.com>
2023-04-26 16:56:52 +0530

    Done till 50

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleApp.Controllers
+{
+    
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("api/get-all")]
+        public string GetAll()
+        {
+            return "Hello from get all";
+        }
+
+        [Route("api/get-all-authors")]
+        public string GetAllAuthors() 
+        {
+            return "Hello from get all Authors";
+        }
+
+        [Route("books/{id}")]
+        public string GetAuthorsId(int id)
+        {
+            return "hello from " + id;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
